e7b61e0f7baa3581513cc19926612999e00b15b1
Serj Danyliuk <serjdanyliuk@gmail.com>
2019-02-25 18:19:15 +0200

    a general improvement

--- app/js/main.js
+++ app/js/main.js
@@ -2,18 +2,7 @@ $(document).ready(function() {
     $(".menu-icon i").on("click", function() {
         $("nav ul").toggleClass("showing");
     });
-});
 
-$(window).on("scroll", function() {
-    if($(window).scrollTop()) {
-        $("nav").addClass("black");
-    }
-    else {
-        $("nav").removeClass("black");
-    }
-});
-
-$(document).ready(function() {
     $('.mySlider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -24,6 +13,15 @@ $(document).ready(function() {
         autoplay: true,
         autoplaySpeed: 3000,
     });
+});
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+        $("nav").addClass("black");
+    }
+    else {
+        $("nav").removeClass("black");
+    }
 });
 
 $('.scroll-btn').click(function(){
